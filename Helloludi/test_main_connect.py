2016ccb46c4fab41e2b001c90f05acf7b5baf5b6
Tase youn <107098737+windnoth@users.noreply.github.com>
2023-07-25 08:11:09 +0900

    Update test_main_connect.py

--- Helloludi/test_main_connect.py
+++ Helloludi/test_main_connect.py
@@ -7,112 +7,98 @@ import time
 import requests
 import HtmlTestRunner
 
+import sys
+sys.path.append('HomepageQA/Setup')
+sys.path.append('HomepageQA/Pagefactory')
+sys.path.append('HomepageQA/Config')
 
-class mainpage(unittest.TestCase, unittest.TestResult):
+from Setup import setUp
+from Locator import Mainpage_locators
+from Config_class import Mainsites
+from Config_class import Config
+
+class mainpage(setUp,Mainpage_locators,Mainsites,Config):
 # 크롬, 최대화, 접속
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
-        self.driver.implicitly_wait(10)
-        self.driver.get("https://www.helloludi.com")
+
            
 # 메인 페이지에서 로고 클릭 시 URL 이동 및 확인, Status Code 200 비교
     def test_001_Mainpage_Check_Url(self):
                 
-        self.driver.find_element(By.XPATH, '//*[@id="header_warp"]/div/div[2]/div[1]').click()
+        self.Main001_btn.click_button()
        
         get_url = self.driver.current_url
-        site1 = "https://www.helloludi.com/"
-
         response = requests.get(get_url)
-
-        self.assertTrue(get_url == site1, response.status_code == 200)
+        
+        self.assertTrue(get_url == Mainsites.Main001_site, response.status_code == 200)
 
 #메인 페이지에서 헬로루디 클릭 시 URL 이동 및 확인, Status Code 200 비교
     def test_002_Introduce_Check_Url(self):
 
-        self.driver.find_element(By.XPATH, '//*[@id="header_warp"]/div/div[2]/div[3]/ul/li[1]').click()
-
+        self.Main002_btn.click_button()
+       
         get_url = self.driver.current_url
-        site2 = "https://www.helloludi.com/main/index.php"
-
         response = requests.get(get_url)
-
-        self.assertTrue(get_url == site2, response.status_code == 200)
+        
+        self.assertTrue(get_url == Mainsites.Main002_site, response.status_code == 200)
 
 #메인 페이지에서 커리큘럼 클릭 시 URL 이동 및 확인  
     def test_003_Curriculum_Check_Url(self):
 
-        self.driver.find_element(By.XPATH, '//*[@id="header_warp"]/div/div[2]/div[3]/ul/li[2]').click()
-
+        self.Main003_btn.click_button()
+       
         get_url = self.driver.current_url
-        site3 = "https://www.helloludi.com/main/curriculum.php"
-
         response = requests.get(get_url)
-
-        self.assertTrue(get_url == site3, response.status_code == 200)
+        
+        self.assertTrue(get_url == Mainsites.Main003_site, response.status_code == 200)
 
 #메인 페이지에서 학습후기 클릭 시 URL 이동 및 확인   
     def test_004_Confirmedreview_Check_Url(self):
         
-        self.driver.find_element(By.XPATH, '//*[@id="header_warp"]/div/div[2]/div[3]/ul/li[3]').click()
-
+        self.Main004_btn.click_button()
+       
         get_url = self.driver.current_url
-        site4 = "https://www.helloludi.com/board/list.php?bdId=confirmedreview"
-
         response = requests.get(get_url)
-
-        self.assertTrue(get_url == site4, response.status_code == 200)
+        
+        self.assertTrue(get_url == Mainsites.Main004_site, response.status_code == 200)
 
 #메인 페이지에서 루디보드 클릭 시 URL 이동 및 확인
     def test_005_Ludiboard_Check_Url(self):
 
-        self.driver.find_element(By.XPATH, '//*[@id="header_warp"]/div/div[2]/div[3]/ul/li[3]').click()
-
+        self.Main005_btn.click_button()
+       
         get_url = self.driver.current_url
-        site5 = "https://www.helloludi.com/board/list.php?bdId=confirmedreview"
-
         response = requests.get(get_url)
         
-        self.assertTrue(get_url == site5, response.status_code == 200)
+        self.assertTrue(get_url == Mainsites.Main005_site, response.status_code == 200)
 
 # 메인 페이지에서 학부모페이지 클릭 후 로그인을 거쳐 다시 한번 클릭 했을 때 URL 확인
     def test_006_Mypage_Check_Url(self):
 
-        self.driver.find_element(By.XPATH, '//*[@id="header_warp"]/div/div[2]/div[3]/ul/li[5]').click()
+        self.Main006_1_btn.click_button()
         
-        textbox_id = self.driver.find_element(By.XPATH, '//*[@id="loginId"]')
-        textbox_id.click()
-        textbox_id.send_keys("@@@") /사용자 ID
-
-        textbox_pw = self.driver.find_element(By.XPATH, '//*[@id="loginPwd"]')
-        textbox_pw.click()
-        textbox_pw.send_keys("@@@") /사용자 PW
-
-        self.driver.find_element(By.XPATH, '//*[@id="formLogin"]/div[1]/div[2]/button').click()
-
+        #로그인
+        self.Main006_2_box.set_text(Config.Helloludi_id)
+        self.Main006_3_box.set_text(Config.Helloludi_pw)
+        self.Main006_4_btn.click_button()
+        
         time.sleep(3)
 
         get_url = self.driver.current_url
-        site6 = "https://www.helloludi.com/mypage/index.php"        
-
         response = requests.get(get_url)
 
-        self.assertTrue(get_url == site6, response.status_code == 200)
+        self.assertTrue(get_url == Mainsites.Main006_site, response.status_code == 200)
 
 #상단 굿즈 배너 이동 및 URL 확인
     def test_007_Goods_Check_Url(self):
         
-        self.driver.find_element(By.XPATH, '//*[@id="TopBanner"]/a').click()
-        
+        self.Main007_btn.click_button()
+       
         get_url = self.driver.current_url
-        site7 = "https://www.helloludi.com/goods/goods_list.php?cateCd=004001"
-
         goodsmain_response = requests.get(get_url)
-        
-        time.sleep(2)
 
-        self.assertTrue(get_url == site7, goodsmain_response.status_code == 200)
+        time.sleep(2)
+        
+        self.assertTrue(get_url == Mainsites.Main007_site, goodsmain_response.status_code == 200)
 
 #굿즈 페이지 패스365 URL 및 확인(사전 조건 : test_Goods_Check_Url(self) = None)
     def test_008_Goods_Pass365_Check_Url(self):
@@ -120,18 +106,13 @@ class mainpage(unittest.TestCase, unittest.TestResult):
         goods = self.test_007_Goods_Check_Url()
 
         if goods == None:
-            self.driver.get("https://www.helloludi.com/goods/goods_list.php?cateCd=004001")
-            self.driver.find_element(By.XPATH, '//*[@id="wrap"]/div[3]/div/div/div[2]/div[3]/div/div[1]/ul/li[1]/div[1]').click()
+            self.driver.get(Mainsites.Main007_site)
+            self.Main008_btn.click_button()
 
-            get_url = self.driver.current_url
-            site8 = "https://www.helloludi.com/goods/goods_view.php?goodsNo=1000000000"
-            
+            get_url = self.driver.current_url           
             pass_365_response = requests.get(get_url)
 
-        else:
-            goodsmain_response = requests.get(get_url)
-
-        self.assertTrue(get_url == site8, pass_365_response.status_code == 200)
+        self.assertTrue(get_url == Mainsites.Main008_site, pass_365_response.status_code == 200)
 
 #굿즈 페이지 패스300 URL 및 확인(사전 조건 : test_Goods_Check_Url(self) = None)
     def test_009_Goods_Pass300_Check_Url(self):
@@ -139,18 +120,13 @@ class mainpage(unittest.TestCase, unittest.TestResult):
         goods = self.test_007_Goods_Check_Url()
                 
         if goods == None:
-            self.driver.get("https://www.helloludi.com/goods/goods_list.php?cateCd=004001")
-            self.driver.find_element(By.XPATH, '//*[@id="wrap"]/div[3]/div/div/div[2]/div[3]/div/div[1]/ul/li[2]/div[1]').click()
+            self.driver.get(Mainsites.Main007_site)
+            self.Main009_btn.click_button()
 
             get_url = self.driver.current_url
-            site9 = "https://www.helloludi.com/goods/goods_view.php?goodsNo=1000000010"
-            
             pass_300_response = requests.get(get_url)
 
-        else : 
-            goodsmain_response = requests.get(get_url)
-
-        self.assertTrue(get_url == site9, pass_300_response.status_code == 200)
+        self.assertTrue(get_url == Mainsites.Main009_site, pass_300_response.status_code == 200)
         
 #굿즈 페이지 패스30 URL 및 확인(사전 조건 : test_Goods_Check_Url(self) = None)
     def test_010_Goods_Pass30_Check_Url(self):
@@ -158,21 +134,13 @@ class mainpage(unittest.TestCase, unittest.TestResult):
         goods = self.test_007_Goods_Check_Url()
         
         if goods == None:
-            self.driver.get("https://www.helloludi.com/goods/goods_list.php?cateCd=004001")
-            self.driver.find_element(By.XPATH, '//*[@id="wrap"]/div[3]/div/div/div[2]/div[3]/div/div[1]/ul/li[3]/div[1]').click()
+            self.driver.get(Mainsites.Main007_site)
+            self.Main010_btn.click_button()
 
-            get_url = self.driver.current_url
-            site10 = "https://www.helloludi.com/goods/goods_view.php?goodsNo=1000000002"
-            
+            get_url = self.driver.current_url          
             pass_30_response = requests.get(get_url)
 
-        else : 
-            goodsmain_response = requests.get(get_url)
-
-        self.assertTrue(get_url == site10, pass_30_response.status_code == 200)
-
-    def tearDown(self):
-        self.driver.quit()
+        self.assertTrue(get_url == Mainsites.Main010_site, pass_30_response.status_code == 200)
 
 if __name__ == "__main__":
     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(open_in_browser=True))
